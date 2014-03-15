03713cff694acdd53d6da76d27e1d931111b03a5
Peter.Iliev@javelingroup.com <peteriliev@gmail.com>
2014-03-15 11:12:10 +0200

    Initial commit

---
+++ merck-uber-basket/src/main/java/com/merck/uber/basket/App.java
@@ -0,0 +1,17 @@
+package com.merck.uber.basket;
+
+import java.util.List;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    List<UberPLI> fooMechanism(final List<OutputItem> erpOutput){
+    	
+    	
+    	return null;
+    }
+}
